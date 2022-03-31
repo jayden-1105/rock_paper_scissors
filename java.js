9795ca4eb8d27f9518eb61b2eefe21759e4999e3
Jayden Jones <jones.jaydenm@gmail.com>
2022-03-31 19:35:12 -0400

    Added function to determine winner. Created function playRound that takes the computer and player inputs and determines the winner. Tracks who won and how many wins they have. Also added game function that loops through playRound 5 times and takes new user and computer input. Added prompt for user input.

--- java.js
+++ java.js
@@ -4,6 +4,67 @@ function computerPlay() {
     return computerChoice;
 }
 
-function playerSelection() {
+
+let computerSelection = computerPlay();
+let result = window.prompt("rock, paper or scissor");
+let playerSelection = result.toLowerCase();
+let playerWins = 0;
+let computerWins = 0;
+
+function playRound(playerSelection, computerSelection) {
+    console.log(computerSelection);
+    console.log(playerSelection);
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+        console.log(`Player wins: ${playerWins}`);
+        console.log(`Computer wins: ${computerWins}`);
+    }
+    if (playerSelection == "rock" && computerSelection == 'paper') {
+        console.log("Computer wins. Paper beats rock.");
+        computerWins += 1;
+        console.log(`Player wins: ${playerWins}`);
+        console.log(`Computer wins: ${computerWins}`);
+    }
+    if (playerSelection == "rock" && computerSelection == "scissor") {
+        console.log("You win. Rock beats scissor.");
+        playerWins += 1;
+        console.log(`Player wins: ${playerWins}`);
+        console.log(`Computer wins: ${computerWins}`);
+    }
+    if (playerSelection == "paper" && computerSelection == "scissor") {
+        console.log("Computer wins. Scissor beats paper.");
+        computerWins += 1;
+        console.log(`Player wins: ${playerWins}`);
+        console.log(`Computer wins: ${computerWins}`);
+    }
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("You win. Paper beats rock.");
+        playerWins += 1;
+        console.log(`Player wins: ${playerWins}`);
+        console.log(`Computer wins: ${computerWins}`);
+    }
+    if (playerSelection == "scissor" && computerSelection == "rock") {
+        console.log("Computer wins. Rock beats scissor.");
+        computerWins += 1;
+        console.log(`Player wins: ${playerWins}`);
+        console.log(`Computer wins: ${computerWins}`);
+    }
+    if (playerSelection == "scissor" && computerSelection == "paper") {
+        console.log("You win. Scissor beats paper.");
+        playerWins += 1;
+        console.log(`Player wins: ${playerWins}`);
+        console.log(`Computer wins: ${computerWins}`);
+    }
 
 }
+
+function game() {
+    for (let i = 0; i < 5; i++){
+        playRound(playerSelection, computerSelection);
+        computerSelection = computerPlay();
+        result = window.prompt("rock, paper or scissor");
+        playerSelection = result.toLowerCase();
+    }
+}
+
+game();
