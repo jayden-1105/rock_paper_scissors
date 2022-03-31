1a72f5efab2c42beb55fa390fd7f7c8ae9366585
Jayden Jones <jones.jaydenm@gmail.com>
2022-03-31 18:59:39 -0400

    Added text box and submit button for player selection

--- java.js
+++ java.js
@@ -1,6 +1,9 @@
 function computerPlay() {
     const choices = ["rock", "paper", "scissor"];
     let computerChoice = choices[Math.floor(Math.random()*3)];
-    console.log(computerChoice);
     return computerChoice;
+}
+
+function playerSelection() {
+
 }
