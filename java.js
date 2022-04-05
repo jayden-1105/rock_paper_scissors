ba5c6046fc691f78d43ce5c3ea5047d53c286f79
Jayden Jones <jones.jaydenm@gmail.com>
2022-04-05 13:02:32 -0400

    Made a lot of changes - details below Should have done a commit before this. Added in the rock paper scissor buttons as images. Formatted the UI to look better. Added in the event listeners for the buttons to run playRound() when clicked. Streamlined the playRound function.

--- java.js
+++ java.js
@@ -4,10 +4,7 @@ function computerPlay() {
     return computerChoice;
 }
 
-
 let computerSelection = computerPlay();
-let result = window.prompt("rock, paper or scissor");
-let playerSelection = result.toLowerCase();
 let playerWins = 0;
 let computerWins = 0;
 
@@ -19,68 +16,60 @@ function playRound(playerSelection, computerSelection) {
         console.log(`Player wins: ${playerWins}`);
         console.log(`Computer wins: ${computerWins}`);
     }
-    else if (playerSelection == "rock" && computerSelection == 'paper') {
+    if (playerSelection == "rock" && computerSelection == 'paper') {
         console.log("Computer wins. Paper beats rock.");
         computerWins += 1;
         console.log(`Player wins: ${playerWins}`);
         console.log(`Computer wins: ${computerWins}`);
     }
-    else if (playerSelection == "rock" && computerSelection == "scissor") {
+    if (playerSelection == "rock" && computerSelection == "scissor") {
         console.log("You win. Rock beats scissor.");
         playerWins += 1;
         console.log(`Player wins: ${playerWins}`);
         console.log(`Computer wins: ${computerWins}`);
     }
-    else if (playerSelection == "paper" && computerSelection == "scissor") {
+    if (playerSelection == "paper" && computerSelection == "scissor") {
         console.log("Computer wins. Scissor beats paper.");
         computerWins += 1;
         console.log(`Player wins: ${playerWins}`);
         console.log(`Computer wins: ${computerWins}`);
     }
-    else if (playerSelection == "paper" && computerSelection == "rock") {
+    if (playerSelection == "paper" && computerSelection == "rock") {
         console.log("You win. Paper beats rock.");
         playerWins += 1;
         console.log(`Player wins: ${playerWins}`);
         console.log(`Computer wins: ${computerWins}`);
     }
-    else if (playerSelection == "scissor" && computerSelection == "rock") {
+    if (playerSelection == "scissor" && computerSelection == "rock") {
         console.log("Computer wins. Rock beats scissor.");
         computerWins += 1;
         console.log(`Player wins: ${playerWins}`);
         console.log(`Computer wins: ${computerWins}`);
     }
-    else if (playerSelection == "scissor" && computerSelection == "paper") {
+    if (playerSelection == "scissor" && computerSelection == "paper") {
         console.log("You win. Scissor beats paper.");
         playerWins += 1;
         console.log(`Player wins: ${playerWins}`);
         console.log(`Computer wins: ${computerWins}`);
     }
-    else {
-        console.log("You did not choose a valid option. You lose");
-        computerWins += 1;
-        console.log(`Player wins: ${playerWins}`);
-        console.log(`Computer wins: ${computerWins}`);
-    }
-
 }
 
-function game() {
-    for (let i = 0; i < 5; i++){
-        playRound(playerSelection, computerSelection);
-        computerSelection = computerPlay();
-        result = window.prompt("rock, paper or scissor");
-        playerSelection = result.toLowerCase();
-    }
-}
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', function() {
+    playRound('rock', computerSelection);
+    computerSelection = computerPlay();
+});
 
-game();
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', function() {
+    playRound('paper', computerSelection);
+    computerSelection = computerPlay();
 
-if (computerWins === playerWins) {
-    console.log("it's a draw");
-}
-else if (computerWins > playerWins) {
-    console.log("computer wins");
-}
-else {
-    console.log("you win");
-}
+});
+const scissor = document.querySelector('.scissor');
+scissor.addEventListener('click', function() {
+    playRound('scissor', computerSelection);
+    computerSelection = computerPlay();
+});
+
+const scoreboard = document.createElement("p");
