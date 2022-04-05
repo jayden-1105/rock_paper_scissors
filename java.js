106702c8e6d0b0708d1fe1406e3a9e6300d6137e
Jayden Jones <jones.jaydenm@gmail.com>
2022-04-05 19:45:33 -0400

    Streamlined UI for buttons and score display. Added in some hover css for the buttons to improve UI. Added code to display the winner as the first to five. Added some CSS to better display the final winner at the end.

--- java.js
+++ java.js
@@ -9,52 +9,36 @@ let playerWins = 0;
 let computerWins = 0;
 
 function playRound(playerSelection, computerSelection) {
-    console.log(computerSelection);
-    console.log(playerSelection);
     if (playerSelection === computerSelection) {
-        console.log("It's a tie!");
-        console.log(`Player wins: ${playerWins}`);
-        console.log(`Computer wins: ${computerWins}`);
-        //scoreboard.textContent = `Player wins: ${playerWins}, Computer wins: ${computerWins}`);
+        gameResult.textContent = "It's a tie!";
     }
     if (playerSelection == "rock" && computerSelection == 'paper') {
-        console.log("Computer wins. Paper beats rock.");
+        gameResult.textContent = "Computer wins. Paper beats rock.";
         computerWins += 1;
-        console.log(`Player wins: ${playerWins}`);
-        console.log(`Computer wins: ${computerWins}`);
-        
     }
     if (playerSelection == "rock" && computerSelection == "scissor") {
-        console.log("You win. Rock beats scissor.");
+        gameResult.textContent = "You win. Rock beats scissor.";
         playerWins += 1;
-        console.log(`Player wins: ${playerWins}`);
-        console.log(`Computer wins: ${computerWins}`);
     }
     if (playerSelection == "paper" && computerSelection == "scissor") {
-        console.log("Computer wins. Scissor beats paper.");
+        gameResult.textContent = "Computer wins. Scissor beats paper.";
         computerWins += 1;
-        console.log(`Player wins: ${playerWins}`);
-        console.log(`Computer wins: ${computerWins}`);
     }
     if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("You win. Paper beats rock.");
+        gameResult.textContent = "You win. Paper beats rock.";
         playerWins += 1;
-        console.log(`Player wins: ${playerWins}`);
-        console.log(`Computer wins: ${computerWins}`);
     }
     if (playerSelection == "scissor" && computerSelection == "rock") {
-        console.log("Computer wins. Rock beats scissor.");
+        gameResult.textContent = "Computer wins. Rock beats scissor.";
         computerWins += 1;
-        console.log(`Player wins: ${playerWins}`);
-        console.log(`Computer wins: ${computerWins}`);
     }
     if (playerSelection == "scissor" && computerSelection == "paper") {
-        console.log("You win. Scissor beats paper.");
+        gameResult.textContent = "You win. Scissor beats paper.";
         playerWins += 1;
-        console.log(`Player wins: ${playerWins}`);
-        console.log(`Computer wins: ${computerWins}`);
     }
-    scoreboard.textContent = `Player wins: ${playerWins} \nComputer wins: ${computerWins}`;
+    playerScore.textContent = `Player score: ${playerWins}`;
+    computerScore.textContent = `Computer score: ${computerWins}`;
+    checkScore(computerWins, playerWins);
 }
 
 const rock = document.querySelector('.rock');
@@ -75,5 +59,17 @@ scissor.addEventListener('click', function() {
     computerSelection = computerPlay();
 });
 
-const scoreboard = document.querySelector('.scoreboard');
+const computerScore = document.querySelector('.computerScore');
+const playerScore = document.querySelector('.playerScore');
 const gameResult = document.querySelector('.result');
+
+function checkScore(computerScore, playerScore) {
+    if (playerScore >= 5) {
+        gameResult.style.color = "red";
+        gameResult.textContent = "You won! Computers still haven't taken over yet.";
+    }
+    if (computerScore >= 5) {
+        gameResult.style.color = "red";
+        gameResult.textContent = "Computer won. The human species has been surpassed.";
+    }
+}
